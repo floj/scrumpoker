e9f4b889da2906caae0223303137cf56d4ae87ce
floj <floj@users.noreply.github.com>
2026-03-05 23:51:19 +0100

    add global error handler

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -7,5 +7,9 @@ import 'notyf/notyf.min.css';
 
 const app = createApp(App);
 
+app.config.errorHandler = (err, _instance, info) => {
+  console.error(`Unhandled error in ${info}:`, err);
+};
+
 app.use(router);
 app.mount('#app');
