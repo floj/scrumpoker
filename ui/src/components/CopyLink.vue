47434ef38dd43fb4acbef2c90f19489060faff4e
floj <floj@users.noreply.github.com>
2026-03-05 23:52:37 +0100

    simpler room link copy

--- ui/src/components/CopyLink.vue
+++ ui/src/components/CopyLink.vue
@@ -1,42 +1,20 @@
 <template>
-  <div class="input-group fit-content">
-    <input
-      ref="input-link"
-      type="text"
-      name="roomlink"
-      class="form-control"
-      aria-label="Room link"
-      readonly
-      :value="link"
-      @click="selectInput"
-    />
-    <button class="btn btn-outline-secondary" @click="copyToClipboard"><i class="bi bi-clipboard"></i></button>
-  </div>
+  <button class="btn btn-outline-secondary" @click="copyToClipboard">
+    <i class="bi bi-clipboard"></i> Copy room link
+  </button>
 </template>
 
 <script lang="ts" setup>
 import { showToast } from '@/utils/toasts';
-import { useTemplateRef } from 'vue';
+
 const props = defineProps<{ link: string }>();
-
-const inputLink = useTemplateRef('input-link');
-
-function selectInput() {
-  inputLink.value?.select();
-}
 
 async function copyToClipboard() {
   try {
     await navigator.clipboard.writeText(props.link);
     showToast('Link copied', 'success', 1000);
-  } catch (err) {
+  } catch {
     showToast('Failed to copy link');
   }
 }
 </script>
-
-<style scoped>
-.fit-content {
-  width: fit-content;
-}
-</style>
