504b0b6e20710a5916ef5c279b84b35a0ed25020
floj <floj@users.noreply.github.com>
2026-03-11 21:26:41 +0100

    Add Bootstrap and Bootstrap Icons; update styles and configuration

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,9 +1,14 @@
 import { createApp } from 'vue';
 
+import 'bootstrap/scss/bootstrap.scss';
+import "bootstrap-icons/font/bootstrap-icons.scss";
+import { Dropdown } from 'bootstrap';
+
 import App from './App.vue';
 import router from './router';
 
 import 'notyf/notyf.min.css';
+
 
 const app = createApp(App);
 
